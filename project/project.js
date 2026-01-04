3e0416fc88337b58f38162f46281f70a006eaf09
walidattaf21-beep <walidattaf21@gmail.com>
2026-01-04 14:15:02 +0100

    Add files via upload

---
+++ project/project.js
@@ -0,0 +1,31 @@
+document.getElementById('buttonfaq').addEventListener('click', function () {
+    veranderTekst('Om je account te stoppen, ga naar je instellingen en kies "Account verwijderen".');
+});
+
+function veranderTekst(nieuweTekst) {
+    document.getElementById('pfaq').innerHTML = nieuweTekst;
+}
+
+document.getElementById('buttonfaqqq').addEventListener('click', function () {
+    veranderTekst('een abbonoment per maand kost 12,99 als u een jaarabbonoment neemt kost hij 100 euro..');
+});
+
+function veranderTekst(nieuweTekst) {
+    document.getElementById('pfaq').innerHTML = nieuweTekst;
+}
+
+document.getElementById('buttonfaqqqq').addEventListener('click', function () {
+    veranderTekst('bij de gallerij zie je de meest beken series.');
+});
+
+function veranderTekst(nieuweTekst) {
+    document.getElementById('pfaq').innerHTML = nieuweTekst;
+}
+
+document.getElementById('buttonfaqqqqq').addEventListener('click', function () {
+    veranderTekst('als u in google onze website opzoekt kan een optie kiezen om ons te bellen dan zullen wij u verder helpen met uw probleem.');
+});
+
+function veranderTekst(nieuweTekst) {
+    document.getElementById('pfaq').innerHTML = nieuweTekst;
+}

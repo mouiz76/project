ba4785407de1d6eea47e847b7004519ef5d79492
walidattaf21-beep <walidattaf21@gmail.com>
2026-01-20 11:23:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const questions = document.querySelectorAll(".faq-question");
+
+  questions.forEach(question => {
+    question.addEventListener("click", () => {
+      const answer = question.nextElementSibling;
+      answer.style.display =
+        answer.style.display === "block" ? "none" : "block";
+    }); 
+  });
